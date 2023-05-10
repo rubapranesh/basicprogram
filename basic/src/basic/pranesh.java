1e3ebc506b19c8123d8816114b8585c0bae125a2
rubapranesh <87354900+rubapranesh@users.noreply.github.com>
2023-05-10 20:53:39 +0530

    deleted pranesh code.java

--- basic/src/basic/pranesh.java
+++ basic/src/basic/pranesh.java
@@ -4,29 +4,7 @@ import java.util.Scanner;
 
 public class reverse {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		
-		//with buffer
-		String s="ABCD";
-		
-		StringBuffer buff =new StringBuffer();
-		
-		buff.append(s);
-		
-		System.out.println(buff.reverse());
-		System.out.println(s.length());
-		
-		
-		//without buffer
-		  Scanner scanner = new Scanner(System.in);
-		  char[] letters = s.toCharArray();
-		for(int i =letters.length-1;i>=0;i--)
-		{
-			System.out.print(letters[i]);
-		}
 	
-		}
+	
 
 }
